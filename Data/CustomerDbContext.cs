3cec4f5fe574b2aa9ea6858ea598bc133c61bbdf
JeremyE-Dev <eudaly_19@hotmail.com>
2023-05-21 10:35:06 -0500

    add initial model and EF Core migration, create database

---
+++ Data/CustomerDbContext.cs
@@ -0,0 +1,22 @@
+﻿using InsuranceClaimSystem_DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsuranceClaimSystem_DAL.Data
+{
+    public class CustomerDbContext : DbContext
+    {
+
+        public CustomerDbContext(DbContextOptions<CustomerDbContext> options)
+            : base(options) 
+        {
+        }
+
+        public DbSet<Customer> Customers {get; set;}    
+     
+    }
+}
